ff2d511f079ce555e56a1f84f8b0705baec47485
george <malazi140@gmail.com>
2023-09-28 14:35:14 +0300

    Deploy

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/geo-planet/',
-  configureWebpack: {
-    plugins: [new MiniCssExtractPlugin()],
-  }
-})
